951d9b92d6fb80f6f1cc11b32c118018cd170950
Tsubasa Azumagakito <azumag@users.noreply.github.com>
2026-09-08 00:35:11 +0900

    fix: scope CommonJS lint suppression to require call

--- smkc-score-app/__tests__/e2e/run-cdm-acceptance.test.ts
+++ smkc-score-app/__tests__/e2e/run-cdm-acceptance.test.ts
@@ -1,10 +1,8 @@
+type CdmAcceptanceRunner = typeof import('../../e2e/run-cdm-acceptance');
+
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const {
-  CDM_TARGETS,
-  parseCliArgs,
-  resolveCommitSha,
-  runCdmAcceptance,
-} = require('../../e2e/run-cdm-acceptance');
+const cdmAcceptanceRunner = require('../../e2e/run-cdm-acceptance') as CdmAcceptanceRunner;
+const { CDM_TARGETS, parseCliArgs, resolveCommitSha, runCdmAcceptance } = cdmAcceptanceRunner;
 
 describe('CDM preview acceptance evidence runner', () => {
   const commitSha = '0123456789abcdef0123456789abcdef01234567';
